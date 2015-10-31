623e1e8a82c3867e9d23e2f40761f443d07182ac
Ilja Bauer <iljabauer@gmail.com>
2015-10-31 22:24:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-        name='dj-postgresql_readonly',
+        name="dj-postgresql_readonly",
         version='1.0',
         description='Readonly postgres django db backend. Use it if you don't have read access to a database',
         maintainer='cuescience',
